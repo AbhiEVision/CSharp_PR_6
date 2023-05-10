e6a9fa1526856873c4a3f75ca8da3d650df00077
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 14:59:06 +0530

    add : ProcessBusinessLogic.cs file which contain definition of delegate and event.

---
+++ CSharp_PR_6/ProcessBusinessLogic.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp_PR_6
+{
+	public delegate void Notify();
+
+	internal class ProcessBusinessLogic
+	{
+		public event Notify ProcessCompleted;
+
+		public void StartProcess()
+		{
+			Console.WriteLine("Process Started...!");
+
+			Thread.Sleep(1000);
+
+			OnProcessCompleted();
+		}
+
+		protected virtual void OnProcessCompleted()
+		{
+			if (ProcessCompleted != null)
+			{
+				ProcessCompleted();
+			}
+		}
+	}
+}
